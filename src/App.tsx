28f4cbdb960d33be77bf49f7ed60248a7f46262f
papa-smurff <122695489+papa-smurff@users.noreply.github.com>
2023-03-08 22:40:24 +0600

    setting background color

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import SettingButtons from "./components/SettingButtons";
 
 function App() {
  return (
-  <div className="max-w-sm mx-auto bg-slate-800 text-slate-50 p-4 rounded-lg">
+  <div className="max-w-sm mx-auto mt-10 bg-slate-800 text-slate-50 p-4 rounded-lg">
    <h2 className="text-3xl mb-4">Password Generator</h2>
    <ResultField />
    <RangeSlider />
